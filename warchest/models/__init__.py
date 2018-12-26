1e2591331b818691c554eb8acac73caf514cdef1
Kaleb Pomeroy <kpomeroy@mailgun.com>
2018-12-26 14:58:05 -0600

    Make mongo configurable

--- warchest/models/__init__.py
+++ warchest/models/__init__.py
@@ -1,4 +1,6 @@
+import os
 import mongoengine
 from warchest.models.game import Game  # NOQA
 
-mongoengine.connect('warchest')
+
+mongoengine.connect('warchest', host=os.environ.get("MONGODB_URI", None))

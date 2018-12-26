86b35afc27da33f8d6859c27976c55b0ab512b45
Kaleb Pomeroy <kpomeroy@mailgun.com>
2018-12-26 15:02:49 -0600

    Use custom port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from warchest import app
 from flask_cors import CORS
 
@@ -9,4 +10,4 @@ from warchest.routes import games, clients, draft, actions  # NOQA
 if __name__ == '__main__':
 
     CORS(app)
-    app.run(debug=True, port=3030)
+    app.run(debug=True, port=os.get("PORT", 3030))
